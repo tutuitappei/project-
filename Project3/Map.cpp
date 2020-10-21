0f508c95cc838ae18f36e9347f4d91f3aeb9009c
tutuitappei <akuryoutukaino@yahoo.co.jp>
2020-10-21 11:14:46 +0900

    Map.cpp

--- Project3/Map.cpp
+++ Project3/Map.cpp
@@ -1,7 +1,27 @@
+#include<DxLib.h>
+#include <iostream>
+#include <string>
 #include "Map.h"
+#include"rapidxml-1.13/rapidxml.hpp"
+#include"rapidxml-1.13/rapidxml_utils.hpp"// rapidxml::file
+
+using namespace std;
+namespace rapid = rapidxml;
 
 Map::Map()
 {
+	rapid::xml_document<> doc;
+	rapid::file<> input("./map/untitled.tmx");
+	doc.parse<0>(input.data());
+
+	rapid::xml_node<>* node = doc.first_node("map");
+	for (rapid::xml_node<>* child = node->first_node();
+		child != nullptr;
+		child = child->next_sibling()) {
+		cout << child->name() << " : [" << child->value() << "]\n";
+	}
+
+
 	_maplayer = Layer::Bg;
 	_mapchip = 32;
 	_mapPos.x = 0;
