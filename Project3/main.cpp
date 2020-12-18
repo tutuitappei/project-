bd4b113dc15f1bfe3b86eb22ac806f863d6a9440
tutuitappei <akuryoutukaino@yahoo.co.jp>
2020-12-18 13:31:09 +0900

    masukippa

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -4,14 +4,16 @@
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {		SetGraphMode(896, 640, 16);								// 640*480ÄŞ¯ÄA65536FÓ°ÄŞ‚Éİ’è
 		ChangeWindowMode(true);									// true:window false:ÌÙ½¸Ø°İ
-
+		
 		SetWindowText("1916024");
 		if (DxLib_Init() == -1)									// DX×²ÌŞ×Ø‚Ì‰Šú‰»ˆ—
 		{
 			return false;
 		}
+		SetAlwaysRunFlag(true);
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
+
 		lpScene.Run();
 		ScreenFlip();
 	}
